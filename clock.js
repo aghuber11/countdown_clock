8ea8333e9736e468f4280143c1791bf4bc2cbddf
Web Design <webdesigner@The-Ghost-of-Megan-Rigysby.local>
2018-09-14 10:19:11 -0700

    adding comments

--- clock.js
+++ clock.js
@@ -1,12 +1,18 @@
-function getTimeRemaining(endtime) {
-
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+/*
+Calculate the time remaining
+============================
+Receiving the variable deadline. This function parses both the endtime and the current time and returns them.
+VARS BEING PASSED
+Deadline - line 56
+*/
+function calculateRemainingTime(endtime) {
+  var time = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((time / 1000) % 60);
+  var minutes = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(time / (1000 * 60 * 60 * 24));
   return {
-    'total': t,
+    'total': time,
     'days': days,
     'hours': hours,
     'minutes': minutes,
@@ -14,28 +20,41 @@ function getTimeRemaining(endtime) {
   };
 }
 
+/*
+Initialize clock
+================
+This is the initial function that runs. It accepts param 'clockdiv' and the var deadline from line 55 / 56
+This function will loop top to bottom if the condition t.total is not equal to 0 (when local time is the same as the deadline).
+*/
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
   var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
-
   function updateClock() {
-    var t = getTimeRemaining(endtime);
+    var time = calculateRemainingTime(endtime);
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    daysSpan.innerHTML = time.days;
+    hoursSpan.innerHTML = ('0' + time.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + time.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + time.seconds).slice(-2);
 
-    if (t.total <= 0) {
+    if (time.total <= 0) {
       clearInterval(timeinterval);
+      let clock1 = document.getElementById("clock_h1");
+      let transition = document.getElementById('clockdiv');
+      clock1.innerHTML = "Time's up!";
+      transition.style.visibility = "hidden";
+      transition.style.opacity = "0";
     }
   }
   updateClock();
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date("9/18/18")));
+// Set the date on the below line. This parses the date provided and sends it for use in calculateRemainingTime (line 8)
+//DOCUMENTATION FOR DATE OBJECT
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+var deadline = new Date(Date.parse(new Date("September 14, 2018 11:30:00")));
 initializeClock('clockdiv', deadline);
